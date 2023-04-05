7a3db0d53f8d4cf06a81f82d65873140358a22eb
Asad414 <79409705+Asad414@users.noreply.github.com>
2023-04-06 01:23:02 +0500

    update key 3

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ def getFomAI(que):
     import openai
 
     key = os.environ.get("MY_VARIABLE")
-    # openai.api_key = "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"
+    openai.api_key = key
 
-    # response = openai.Completion.create(
-    #     model="text-davinci-003",
-    #     prompt= "You are a tarot card reader and I am your client. please describe the imagery, symbolism, and meaning of each card in the following celtic spread in the context of “"+que+"” and the cards:  The Present: Six of Wands, The Challenge: Ace of Pentacles, The Past: Page of Swords, The Future: Wheel of Fortune, Above: Two of Cups, Below: The Sun, Advice: Three of Pentacles, External Influences: The Empress, Hopes and Fears: Eight of Swords, Outcome: Knight of Swords. Add <br><br> tag when a card is complete.",
-    #     temperature=0.7,
-    #     max_tokens=1000,
-    #     top_p=1,
-    #     frequency_penalty=0,
-    #     presence_penalty=0
-    # )
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= "You are a tarot card reader and I am your client. please describe the imagery, symbolism, and meaning of each card in the following celtic spread in the context of “"+que+"” and the cards:  The Present: Six of Wands, The Challenge: Ace of Pentacles, The Past: Page of Swords, The Future: Wheel of Fortune, Above: Two of Cups, Below: The Sun, Advice: Three of Pentacles, External Influences: The Empress, Hopes and Fears: Eight of Swords, Outcome: Knight of Swords. Add <br><br> tag when a card is complete.",
+        temperature=0.7,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
 
-    # ans = response['choices'][0]['text']
-    return key
+    ans = response['choices'][0]['text']
+    return ans
 
 @app.route('/')
 def index():
